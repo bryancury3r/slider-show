e5c863780059944608156b408cc351a7a27588d9
bryancury3r <gabriel_bryancury@outlook.com>
2022-04-11 21:09:47 -0300

    add script

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let totalSlides = document.querySelectorAll('.slider-item').length;
 
-document.querySelector('.slider-width').style.width = `calc(100vw * ${totalSlides})`;
-document.querySelector('.slider-controls')
+document.querySelector('.slider-width').style.width = 
+  `calc(100vw * ${totalSlides})`;
+document.querySelector('.slider-controls').style.height = 
+  `${document.querySelector('.slider').clientHeight}`;
